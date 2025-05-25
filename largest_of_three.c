e99177af510e1dea1b815f0411708569d67cbf80
ismael-s-soon <docteurpapa10@gmail.com>
2025-05-25 23:46:43 +0100

    Update largest_of_three.c

--- largest_of_three.c
+++ largest_of_three.c
@@ -1,25 +1,33 @@
-#include <stdio.h> // Inclusion de la bibliothèque standard pour les entrées/sorties
+#include <stdio.h> // #include : directive pour inclure une bibliothèque
+                   // <stdio.h> : standard input/output, pour printf et scanf
 
-int main() {
-    int a, b, c; // Déclaration de trois variables entières a, b et c
+int main() { // int : type de retour, main() : point d’entrée du programme
+    int a, b, c; // int : déclare des entiers, a, b, c : variables pour stocker 3 nombres
 
-    // Lecture de trois nombres entiers entrés par l'utilisateur
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c); 
+    // scanf : lit les entrées clavier
+    // "%d %d %d" : format pour lire 3 entiers
+    // &a, &b, &c : adresses où stocker les valeurs saisies
 
-    // Vérifie si 'a' est plus grand ou égal à 'b' ET à 'c'
-    if (a >= b && a >= c) {
-        // Si c'est vrai, affiche que 'a' est le plus grand
-        printf("Le plus grand nombre est : %d\n", a);
-    } 
-    // Sinon, vérifie si 'b' est plus grand ou égal à 'a' ET à 'c'
-    else if (b >= a && b >= c) {
-        // Si c'est vrai, affiche que 'b' est le plus grand
-        printf("Le plus grand nombre est : %d\n", b);
-    } 
-    // Si ni 'a' ni 'b' ne sont les plus grands, alors 'c' est le plus grand
-    else {
-        printf("Le plus grand nombre est : %d\n", c);
+    if (a >= b && a >= c) { 
+    // if : test une condition
+    // a >= b : vérifie si a est plus grand ou égal à b
+    // && : opérateur logique ET
+    // a >= c : vérifie si a est aussi plus grand ou égal à c
+        printf("Le plus grand nombre est : %d\n", a); 
+        // printf : affiche du texte + valeur
+        // %d : affiche a
+        // \n : retour à la ligne
+    } else if (b >= a && b >= c) { 
+    // else if : sinon, teste une autre condition
+    // b >= a && b >= c : vérifie si b est le plus grand ou égal aux autres
+        printf("Le plus grand nombre est : %d\n", b); 
+    } else { 
+    // else : tous les autres cas (donc ici c est le plus grand)
+        printf("Le plus grand nombre est : %d\n", c); 
     }
 
-    return 0; // Fin du programme
+    return 0; 
+    // return : fin du programme
+    // 0 : code de sortie indiquant que tout s’est bien passé
 }
